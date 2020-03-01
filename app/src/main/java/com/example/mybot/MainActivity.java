d0533fa90f491de339ebd2a669204cfba9ae6a3e
Rutuja K <rutuja.kanki@gmail.com>
2020-02-29 16:36:36 -0800

    initial

--- app/src/main/java/com/example/mybot/MainActivity.java
+++ app/src/main/java/com/example/mybot/MainActivity.java
@@ -1,81 +1,262 @@
 package com.example.mybot;
 
-import androidx.appcompat.app.AppCompatActivity;
+import android.Manifest;
+import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.AsyncTask;
 import android.os.Bundle;
-import android.widget.TextView;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.firebase.ui.database.FirebaseRecyclerAdapter;
+import com.firebase.ui.database.FirebaseRecyclerOptions;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+
+import ai.api.AIDataService;
 import ai.api.AIListener;
-import ai.api.android.AIService;
+import ai.api.AIServiceException;
 import ai.api.android.AIConfiguration;
-import ai.api.model.AIError;
+import ai.api.android.AIService;
+import ai.api.model.AIRequest;
 import ai.api.model.AIResponse;
 import ai.api.model.Result;
-import android.Manifest;
-import android.content.pm.PackageManager;
-import android.util.Log;
-import android.view.View;
+
 //import android.support.v4.app.ActivityCompat;
-//import android.support.v4.content.ContextCompat;
+//import android.support.v7.app.AppCompatActivity;
+//import android.support.v7.widget.LinearLayoutManager;
+//import android.support.v7.widget.RecyclerView;
 
 
+public class MainActivity<PersonModel> extends AppCompatActivity implements AIListener{
+
+    RecyclerView recyclerView;
+    EditText editText;
+    RelativeLayout addBtn;
+    DatabaseReference ref;
+    FirebaseRecyclerAdapter<ChatMessage,chat_rec> adapter;
+    Boolean flagFab = true;
+
+    private AIService aiService;
 
 
-public class MainActivity extends AppCompatActivity implements AIListener{
-
-    AIService aiService;
-    TextView t;
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected <PeopleVH> void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        t=findViewById(R.id.textView);
 
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.RECORD_AUDIO},1);
 
-        /*int permission = ContextCompat.checkSelfPermission(this, Manifest.permission.RECORD_AUDIO);
-        if (permission != PackageManager.PERMISSION_GRANTED) {
-            makeRequest();
-        }*/
+        recyclerView = (RecyclerView)findViewById(R.id.recyclerView);
+        editText = (EditText)findViewById(R.id.editText);
+        addBtn = (RelativeLayout)findViewById(R.id.addBtn);
 
-        final AIConfiguration config = new AIConfiguration("b4ff2ed344f844a39579644d0754fa07",
+        recyclerView.setHasFixedSize(true);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this);
+        linearLayoutManager.setStackFromEnd(true);
+        recyclerView.setLayoutManager(linearLayoutManager);
+
+        ref = FirebaseDatabase.getInstance().getReference();
+        ref.keepSynced(true);
+
+        final AIConfiguration config = new AIConfiguration("0c01e159babc4349b38eca698bd2f107",
                 AIConfiguration.SupportedLanguages.English,
                 AIConfiguration.RecognitionEngine.System);
+
         aiService = AIService.getService(this, config);
         aiService.setListener(this);
-    }
 
-    /*protected void makeRequest() {
-        ActivityCompat.requestPermissions(this,
-                new String[]{Manifest.permission.RECORD_AUDIO},
-                101);
-    }*/
+        final AIDataService aiDataService = new AIDataService(config);
 
-    @Override
-    public void onRequestPermissionsResult(int requestCode, String permissions[], int[] grantResults) {
-        switch (requestCode) {
-            case 101: {
+        final AIRequest aiRequest = new AIRequest();
 
-                if (grantResults.length == 0 || grantResults[0] != PackageManager.PERMISSION_GRANTED) {
-                } else {
+
+
+        addBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                String message = editText.getText().toString().trim();
+
+                if (!message.equals("")) {
+
+                    ChatMessage chatMessage = new ChatMessage(message, "user");
+                    ref.child("chat").push().setValue(chatMessage);
+
+                    aiRequest.setQuery(message);
+                    new AsyncTask<AIRequest,Void,AIResponse>(){
+
+                        @Override
+                        protected AIResponse doInBackground(AIRequest... aiRequests) {
+                            final AIRequest request = aiRequests[0];
+                            try {
+                                final AIResponse response = aiDataService.request(aiRequest);
+                                return response;
+                            } catch (AIServiceException e) {
+                            }
+                            return null;
+                        }
+                        @Override
+                        protected void onPostExecute(AIResponse response) {
+                            if (response != null) {
+
+                                Result result = response.getResult();
+                                String reply = result.getFulfillment().getSpeech();
+                                ChatMessage chatMessage = new ChatMessage(reply, "bot");
+                                ref.child("chat").push().setValue(chatMessage);
+                            }
+                        }
+                    }.execute(aiRequest);
                 }
-                return;
+                else {
+
+                    aiService.startListening();
+                }
+
+                editText.setText("");
+
             }
-        }
+        });
+
+
+
+        editText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+                ImageView fab_img = (ImageView)findViewById(R.id.fab_img);
+                Bitmap img = BitmapFactory.decodeResource(getResources(),R.drawable.ic_send_white_24dp);
+                Bitmap img1 = BitmapFactory.decodeResource(getResources(),R.drawable.ic_mic_white_24dp);
+
+                if (s.toString().trim().length()!=0 && flagFab){
+                    ImageViewAnimatedChange(MainActivity.this,fab_img,img);
+                    flagFab=false;
+
+                }
+                else if (s.toString().trim().length()==0){
+                    ImageViewAnimatedChange(MainActivity.this,fab_img,img1);
+                    flagFab=true;
+                }
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+
+
+
+        adapter = new FirebaseRecyclerAdapter<ChatMessage, chat_rec>(ChatMessage.class,R.layout.msglist,chat_rec.class,ref.child("chat"))
+        {
+            @Override
+            protected void onBindViewHolder(@NonNull chat_rec viewHolder, int i, @NonNull ChatMessage model) {
+                if (model.getMsgUser().equals("user")) {
+                    viewHolder.rightText.setText(model.getMsgText());
+                    viewHolder.rightText.setVisibility(View.VISIBLE);
+                    viewHolder.leftText.setVisibility(View.GONE);
+                }
+                else {
+                    viewHolder.leftText.setText(model.getMsgText());
+                    viewHolder.rightText.setVisibility(View.GONE);
+                    viewHolder.leftText.setVisibility(View.VISIBLE);
+                }
+            }
+
+            @NonNull
+            @Override
+            public chat_rec onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+                return null;
+            }
+        };
+
+
+        adapter.registerAdapterDataObserver(new RecyclerView.AdapterDataObserver() {
+            @Override
+            public void onItemRangeInserted(int positionStart, int itemCount) {
+                super.onItemRangeInserted(positionStart, itemCount);
+
+                int msgCount = adapter.getItemCount();
+                int lastVisiblePosition = linearLayoutManager.findLastCompletelyVisibleItemPosition();
+
+                if (lastVisiblePosition == -1 ||
+                        (positionStart >= (msgCount - 1) &&
+                                lastVisiblePosition == (positionStart - 1))) {
+                    recyclerView.scrollToPosition(positionStart);
+
+                }
+
+            }
+        });
+
+        recyclerView.setAdapter(adapter);
+
+
     }
 
+    private Context getActivity() {
+    }
 
-    public void buttonClicked(View view){
-        aiService.startListening();
+    public void ImageViewAnimatedChange(Context c, final ImageView v, final Bitmap new_image) {
+        final Animation anim_out = AnimationUtils.loadAnimation(c, R.anim.zoom_out);
+        final Animation anim_in  = AnimationUtils.loadAnimation(c, R.anim.zoom_in);
+        anim_out.setAnimationListener(new Animation.AnimationListener()
+        {
+            @Override public void onAnimationStart(Animation animation) {}
+            @Override public void onAnimationRepeat(Animation animation) {}
+            @Override public void onAnimationEnd(Animation animation)
+            {
+                v.setImageBitmap(new_image);
+                anim_in.setAnimationListener(new Animation.AnimationListener() {
+                    @Override public void onAnimationStart(Animation animation) {}
+                    @Override public void onAnimationRepeat(Animation animation) {}
+                    @Override public void onAnimationEnd(Animation animation) {}
+                });
+                v.startAnimation(anim_in);
+            }
+        });
+        v.startAnimation(anim_out);
     }
 
     @Override
-    public void onResult(AIResponse result) {
+    public void onResult(ai.api.model.AIResponse response) {
 
-        Log.d("anu",result.toString());
-        Result result1=result.getResult();
-        t.setText("Query "+result1.getResolvedQuery()+" action: "+result1.getFulfillment().getSpeech());
+
+        Result result = response.getResult();
+
+        String message = result.getResolvedQuery();
+        ChatMessage chatMessage0 = new ChatMessage(message, "user");
+        ref.child("chat").push().setValue(chatMessage0);
+
+
+        String reply = result.getFulfillment().getSpeech();
+        ChatMessage chatMessage = new ChatMessage(reply, "bot");
+        ref.child("chat").push().setValue(chatMessage);
+
+
     }
 
     @Override
-    public void onError(AIError error) {
+    public void onError(ai.api.model.AIError error) {
 
     }
 
@@ -98,8 +279,4 @@ public class MainActivity extends AppCompatActivity implements AIListener{
     public void onListeningFinished() {
 
     }
-
-
-
-
 }
